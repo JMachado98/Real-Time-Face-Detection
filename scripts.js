cb016bb8280d03c16fbbbc6b7b17f781b6f70cad
Johnny Machado <johnnymachado490@gmail.com>
2021-12-23 20:58:09 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -26,5 +26,6 @@ video.addEventListener('play', () => {
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         faceapi.draw.drawDetections(canvas, resizeDetections)
         faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizeDetections)
     }, 100)
 })
